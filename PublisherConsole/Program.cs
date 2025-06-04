9adc4233c3d4cb5f4e619f3cc2a34383703b47e8
Diana Moschetti <dianamoschetti@gmail.com>
2025-06-04 17:09:10 -0300

    EF Core Fundamentals Mod 2

---
+++ PublisherConsole/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+//GetAuthors();
+//AddAuthor();
+//GetAuthors();
+AddAuthorWithBook();
+GetAuthorsWithBooks();
+
+
+    void AddAuthorWithBook()
+{
+    var author = new Author
+    {
+        FirstName = "Julie",
+        LastName = "Lerman"
+    };
+    author.Books.Add(new Book
+    {
+        Title = "Programming Entity Framework",
+        PublishDate = new DateOnly(2009, 1, 1)
+    });
+    author.Books.Add(new Book
+    {
+        Title = "Programming Entity Framework 2nd Ed",
+        PublishDate = new DateOnly(2010, 8, 1)
+    });
+
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthorsWithBooks()
+{
+    using var context = new PubContext();
+
+    var authors = context.Authors.Include(a => a.Books)
+        .ToList();
+
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine("  " + book.Title + " (" + book.PublishDate.Year + ")");
+        }
+    }
+}
+
+    void AddAuthor()
+{
+    var author = new Author
+    {
+        FirstName = "Diana",
+        LastName = "Moschetti"
+    };
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+void GetAuthors()
+{
+    using var context = new PubContext();
+    
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+    
+}
