adf4dbe8cb51eb40631ae9e1e6180b82c5d1022b
DianaMoschetti <dianamoschetti@gmail.com>
2025-06-08 13:31:18 -0300

    EF Core Fundamentals Mod 4

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -22,24 +22,23 @@ using PubContext _context = new();
 //AddSomeMoreAuthors();
 //SkipAndTakeAuthors();
 //SortAuthors();
-QueryAggregate();
 void QueryAggregate()
 {
     //var author = _context.Authors.FirstOrDefault(a => a.LastName == "Lerman");
-    var author = _context.Authors.OrderByDescending(a => a.FirstName).FirstOrDefault(a =>a.LastName == "Lerman");
+    var author = _context.Authors.OrderByDescending(a => a.FirstName).FirstOrDefault(a => a.LastName == "Lerman");
 
     void GetAuthors()
-{
-    using var context = new PubContext();
-
-    var authors = context.Authors.ToList();
-    foreach (var author in authors)
     {
-        Console.WriteLine(author.FirstName + " " + author.LastName);
+        using var context = new PubContext();
+
+        var authors = context.Authors.ToList();
+        foreach (var author in authors)
+        {
+            Console.WriteLine(author.FirstName + " " + author.LastName);
+        }
+
     }
-
 }
-
 void SortAuthors()
 {
     var authorsByLastname = _context.Authors
@@ -66,7 +65,6 @@ void SkipAndTakeAuthors()
         Console.WriteLine();
     }
 }
-
 void QueryFilters()
 {
     //var authors = _context.Authors.Where(a => a.FirstName == "Josie")
@@ -84,7 +82,6 @@ void QueryFilters()
         .ToList();
 
 }
-
 void AddAuthorWithBook()
 {
     var author = new Author
@@ -134,7 +131,6 @@ void AddAuthor()
     context.Authors.Add(author);
     context.SaveChanges();
 }
-
 void AddSomeMoreAuthors()
 {
     _context.Authors.Add(new Author { FirstName = "Rhoda", LastName = "Lerman" });
@@ -143,4 +139,162 @@ void AddSomeMoreAuthors()
     _context.Authors.Add(new Author { FirstName = "Stephen", LastName = "Haunts" });
 
     _context.SaveChanges();
+}
+
+//MODULO 4
+//InsertAuthor();
+void InsertAuthor()
+{
+    var author = new Author
+    {
+        FirstName = "Frank",
+        LastName = "Herbert"
+    };   
+    _context.Authors.Add(author);
+    _context.SaveChanges();
+}
+
+//RetrieveAndUpdateAuthor();
+void RetrieveAndUpdateAuthor()
+{
+    var author = _context.Authors
+        .FirstOrDefault(a => a.FirstName == "Julie" && a.LastName == "Lerman");
+    if (author != null)
+    {
+        author.FirstName = "Lherman";
+        _context.SaveChanges();
+    }
+}
+
+//RetrieveAndUpdateMultipleAuthors();
+void RetrieveAndUpdateMultipleAuthors()
+{
+    var LermanAuthors = _context.Authors
+        .Where(a => a.FirstName == "Lherman")
+        .ToList();
+    foreach (var la in LermanAuthors)
+    {
+        la.FirstName = "Julie";
+    }
+    Console.WriteLine($"Before: \r\n{_context.ChangeTracker.DebugView.ShortView}");
+    _context.ChangeTracker.DetectChanges();
+    Console.WriteLine($"After: \r\n{_context.ChangeTracker.DebugView.ShortView}");
+    _context.SaveChanges();
+}
+
+//VariousOperations();
+void VariousOperations()
+{
+    var author = _context.Authors.Find(1);
+    author.FirstName = "Diana Ines";
+    var newAuthor = new Author { LastName = "Appleman", FirstName = "Dan" };
+    _context.Authors.Add(newAuthor);
+    _context.SaveChanges();
+}
+
+//CoordinatedRetrieveAndUpdateAuthor();
+void CoordinatedRetrieveAndUpdateAuthor()
+{
+    var author = FindThatAuthor(3);
+    if(author?.FirstName == "Julie")
+    {
+        author.FirstName = "Julia";
+        SaveThatAuthor(author);
+    }
+}
+Author FindThatAuthor(int authorId)
+{
+    using var shortLivedContext = new PubContext();
+    return shortLivedContext.Authors.Find(authorId);
+}
+void SaveThatAuthor(Author author)
+{
+    using var anotherShortLivedContext = new PubContext();
+    anotherShortLivedContext.Authors.Update(author);
+    anotherShortLivedContext.SaveChanges();
+}
+
+//DeleteAnAuthor();
+void DeleteAnAuthor()
+{
+    var author = _context.Authors.Find(5);
+    if (author != null)
+    {
+        _context.Authors.Remove(author);
+        _context.SaveChanges();
+    }
+}
+
+//InsertMultpleAuthorsBetter();
+void InsertMultpleAuthorsBetter()
+{
+    var authors = new List<Author>
+    {
+        new Author { FirstName = "Frank", LastName = "Herbert" },
+        new Author { FirstName = "Isaac", LastName = "Asimov" },
+        new Author { FirstName = "Arthur C.", LastName = "Clarke" },
+        new Author { FirstName = "Ray", LastName = "Bradbury" },
+        new Author { FirstName = "Ruth", LastName = "Ozeki" },
+        new Author { FirstName = "Sofia", LastName = "Segovia" },
+        new Author { FirstName = "Ursula K.", LastName = "LeGuin" },
+        new Author { FirstName = "Hugh", LastName = "Howey" },
+        new Author { FirstName = "Isabelle", LastName = "Allende" }
+    };
+    _context.Authors.AddRange(authors);
+    _context.SaveChanges();
+}
+void InsertMultipleAuthors()
+{
+    _context.Authors.AddRange(
+        new Author { FirstName = "Frank", LastName = "Herbert" },
+        new Author { FirstName = "Isaac", LastName = "Asimov" },
+        new Author { FirstName = "Arthur C.", LastName = "Clarke" },
+        new Author { FirstName = "Ray", LastName = "Bradbury" },
+        new Author { FirstName = "Ruth", LastName = "Ozeki" },
+        new Author { FirstName = "Sofia", LastName = "Segovia" },
+        new Author { FirstName = "Ursula K.", LastName = "LeGuin" },
+        new Author { FirstName = "Hugh", LastName = "Howey" },
+        new Author { FirstName = "Isabelle", LastName = "Allende" }
+    );
+    _context.SaveChanges();
+}
+void InsertMultipleAuthorsPassedIn(List<Author> listOfAuthors)
+{
+    _context.Authors.AddRange(listOfAuthors);
+    _context.SaveChanges();
+}
+
+//ExecuteDelete();
+void ExecuteDelete()
+{
+    var deletedId = 11;
+    _context.Authors.Where(a => a.Id == deletedId)
+        .ExecuteDelete();
+}
+
+//ExecuteDeleteWithH();
+void ExecuteDeleteWithH()
+{
+    var count = _context.Authors.Where(a => a.LastName.StartsWith("H"))
+        .ExecuteDelete();
+}
+
+//ExecuteUpdate();
+void ExecuteUpdate()
+{
+    var tenYearsAgo = DateOnly.FromDateTime(DateTime.Now.AddYears(-10));
+    //change price of book older than 10 years to $1.50
+    var oldBookPrice = 1.50m;
+    _context.Books
+        .Where(b => b.PublishDate < tenYearsAgo)
+        .ExecuteUpdate(setters => setters.SetProperty(b => b.BasePrice, oldBookPrice));
+
+    //change all last names to lower case
+    _context.Authors
+        .ExecuteUpdate(setters => setters.SetProperty(a => a.LastName, a => a.LastName.ToLower()));
+    //change all last names back to title case
+    _context.Authors
+        .ExecuteUpdate(setters => setters.SetProperty(
+            a => a.LastName, 
+            a => a.LastName.Substring(0,1).ToUpper() + a.LastName.Substring(1).ToLower()));
 }
